c05cff234c7b06ff3d2c8948d1b553586202073c
Ellen Luiza <ellen.luizasilva93@gmail.com>
2024-10-02 10:05:21 -0300

    Add files via upload
    Cálculo de adição

---
+++ soma/soma.js
@@ -0,0 +1,7 @@
+//EPS
+//Entrada Processamento Saida
+
+var numero = parseInt (prompt("digite um numero: "))
+var numero1 =parseInt (prompt("Digite mais um numero: "))
+var soma = numero + numero1
+alert("A soma total é: "+ soma)
