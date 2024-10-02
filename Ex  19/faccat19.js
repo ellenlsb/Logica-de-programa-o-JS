1a2da98bd5435fca5a7aca6ff2e7a1fa47a5fead
Ellen Luiza <ellen.luizasilva93@gmail.com>
2024-10-01 23:37:06 -0300

    Add files via upload
    Ler dois valores (considere que não serão lidos valores iguais) e escrever o maior deles.

---
+++ Ex  19/faccat19.js
@@ -0,0 +1,8 @@
+let numero1 = parseInt(prompt("Digite o primeiro número: "))
+let numero2 = parseInt(prompt("Digite o segundo número: "))
+
+if (numero1 > numero2){
+    alert ("O maior numero é: " + numero1)
+} else {
+    alert ("O maior numero é: " + numero2)
+}
