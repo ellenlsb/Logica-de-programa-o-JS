c2b9acc3ab310414200a507f422207ceff172589
Ellen Luiza <ellen.luizasilva93@gmail.com>
2024-10-02 09:55:30 -0300

    Add files via upload
    Ler um valor e escrever se é positivo, negativo ou zero.

---
+++ Ex 27/faccat27.js
@@ -0,0 +1,9 @@
+let numero = parseFloat(prompt("Digite um número: "));
+
+if (numero > 0) {
+    alert("Número POSITIVO!");
+} else if (numero < 0) {
+    alert("Número NEGATIVO!");
+} else {
+    alert("Número NEUTRO!");
+}
