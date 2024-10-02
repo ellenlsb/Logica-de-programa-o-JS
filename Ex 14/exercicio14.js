c666d38023fb380ff24fb2604ef2496b5db46dbc
Ellen Luiza <ellen.luizasilva93@gmail.com>
2024-10-01 23:17:41 -0300

    Add files via upload
    Ler um valor e escrever a mensagem É MAIOR QUE 10! se o valor lido for maior que 10, caso 
    contrário escrever NÃO É MAIOR QUE 10!

---
+++ Ex 14/exercicio14.js
@@ -0,0 +1,12 @@
+let n1 = parseFloat(prompt("Escreva um numero: "))
+
+if (n1 > 10) {
+    alert("É MAIOR QUE 10!")
+} else
+    if (n1 < 10) {
+        alert("É MENOR QUE 10!")
+    }
+if (n1 == 10) {
+    alert("É 10")
+}
+
