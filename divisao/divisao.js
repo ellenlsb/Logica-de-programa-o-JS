0881ed185ef90374e4d5f6e8c4657f59a579cb06
Ellen Luiza <ellen.luizasilva93@gmail.com>
2024-10-02 10:07:23 -0300

    Add files via upload
    Cálculo divisão

---
+++ divisao/divisao.js
@@ -0,0 +1,4 @@
+var numero = parseInt(prompt("Digite um numero: "))
+var numero1 = parseInt (prompt("Digite um segundo numero: "))
+var resultado = numero / numero1
+alert("O resultado é: " + resultado)
