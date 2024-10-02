51d4cd4104e41f2d76738ecc1fa314787f858049
Ellen Luiza <ellen.luizasilva93@gmail.com>
2024-10-02 09:58:48 -0300

    Add files via upload

---
+++ Ex 34/faccat34.js
@@ -0,0 +1,10 @@
+var x = parseInt(prompt("Escreva um valor para x"))
+var y = parseInt(prompt("Escreva um valor para y"))
+var z = (x * y + 5)
+if (z <= 0){
+alert("A")}
+else if (z <= 100) {
+    alert("B")
+}else{
+    alert("C")
+}
